6b6ff240b6cc18d35607b5a6720f66187c2abd3b
christopher lee <clee@edx.org>
2017-03-22 19:15:04 -0400

    Fixed bug where other currencies were not calculated in balance

--- utils.py
+++ utils.py
@@ -2,3 +2,12 @@ class bcolors:
     GREEN = '\033[92m'
     RED = '\033[91m'
     END_COLOR = '\033[0m'
+
+
+def print_dw_history(deposits, withdrawals):
+    print "-----Deposit/Withdrawal History-----"
+    print "------------------------------------"
+    print "--Currency=Deposit-Withdrawal=Total-"
+    for currency, deposit in deposits.iteritems():
+        withdrawal = withdrawals[currency] if currency in withdrawals else 0
+        print "{}={}-{}={}".format(currency, deposit, withdrawal, deposit - withdrawal)
